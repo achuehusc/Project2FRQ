ea6a541e72d0967b93837a7b6f2335fa9f37fb32
Aidan Chueh <achueh@usc.edu>
2022-10-06 23:44:20 -0700

    Updated include files

--- race.c
+++ race.c
@@ -1,5 +1,8 @@
 #include "types.h"
 #include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+#include "condvar.h"
 
 //We want Child 1 to execute first, then Child 2, and finally Parent.
 int main() {
